9e7bd71581c241a18213be635f2324b845d79050
crystalclearaspen <crystalclearaspen@gmail.com>
2022-06-04 12:40:38 -0600

    change from crystalclear

--- src/main/java/com/example/restapp/RestAppApplication.java
+++ src/main/java/com/example/restapp/RestAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestAppApplication {
 
     public static void main(String[] args) {
+        // Another change from crystal clear
         SpringApplication.run(RestAppApplication.class, args);
     }
 
