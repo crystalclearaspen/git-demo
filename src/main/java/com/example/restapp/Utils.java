133457db687afb22cb46fcf059107cd32b5a82c8
crystalclearaspen <crystalclearaspen@gmail.com>
2022-06-04 12:57:03 -0600

    Added some dummy method from repo 1

--- src/main/java/com/example/restapp/Utils.java
+++ src/main/java/com/example/restapp/Utils.java
@@ -3,4 +3,8 @@ package com.example.restapp;
 public class Utils {
 
     private String utilString = "sample text";
+
+    public void addedMethod(){
+        System.out.println("This method doesnt do anything");
+    }
 }
